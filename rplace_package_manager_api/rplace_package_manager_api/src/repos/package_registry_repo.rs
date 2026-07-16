3c2971875dde31b3abd52c1ff6e32a7c7f8f6495
rafael <rafapenacho@gmail.com>
2026-07-16 14:18:52 +0100

    registry repo

--- rplace_package_manager_api/rplace_package_manager_api/src/repos/package_registry_repo.rs
+++ rplace_package_manager_api/rplace_package_manager_api/src/repos/package_registry_repo.rs
@@ -1,15 +1,23 @@
 use async_trait::async_trait;
-use anyhow::{Result};
+use anyhow::{Ok, Result};
 
-use crate::{db::{db_provider::PackageRegistryRepo, sqlite_db::SqliteDb}};
+use crate::{db::{db_provider::PackageRegistryRepo, sqlite_db::SqliteDb}, models::registry::package_registry::{PackageRegistry, PackageRegistryCreateDto}};
 
 #[async_trait]
 impl PackageRegistryRepo for SqliteDb {
-    async fn get_registry_by_name(&self, name: String) -> Result<crate::models::registry::package_registry::PackageRegistry> {
-        todo!()
+    async fn get_registry_by_name(&self, name: String) -> Result<PackageRegistry>{
+        let sql = "SELECT * FROM package_registry WHERE package_name = ?;";
+        let registry = sqlx::query_as::<_,PackageRegistry>(sql).bind(name).fetch_one(&self.pool).await?;
+        Ok(registry)
     }
-
-    async fn new_registry(&self, registry: crate::models::registry::package_registry::PackageRegistryCreateDto, user_id: i32) -> Result<crate::models::registry::package_registry::PackageRegistry> {
-        todo!()
+    async fn get_registry_by_id(&self, id: i32) -> Result<PackageRegistry>{
+        let sql = "SELECT * FROM package_registry WHERE id = ?;";
+        let registry = sqlx::query_as::<_,PackageRegistry>(sql).bind(id).fetch_one(&self.pool).await?;
+        Ok(registry)
+    }
+    async fn new_registry(&self, registry: PackageRegistryCreateDto, user_id: i32) -> Result<PackageRegistry>{
+        let sql = "INSERT INTO package_registry (package_name, cretor_id) VALUES (?,?);";
+        let registry = sqlx::query_as::<_, PackageRegistry>(sql).bind(registry.name).bind(user_id).fetch_one(&self.pool).await?;
+        Ok(registry)
     }
 }
